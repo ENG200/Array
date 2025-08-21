9ee7a79d56db7289cd99e9973a55b671ca2a0b9e
AlRushood <AlRushood@DESKTOP-9AI2KOR>
2025-08-21 23:09:11 +0300

    Add 5 to the loop condition

--- Array.java
+++ Array.java
@@ -9,7 +9,7 @@ public class Array {
 	 }
 	 public int occur (int key) {
 		 int count = 0;
-		 for (int i=0; i<A.length; i++) 
+		 for (int i=0; i<A.length + 5; i++) 
 			 if (A[i] == key) count++;
 		 return count;
 		 }
