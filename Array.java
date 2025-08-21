9ef61a84dee77a454c18e8e5a4334e7f75ca614c
AlRushood <AlRushood@DESKTOP-9AI2KOR>
2025-08-21 22:56:24 +0300

    Update the loop condition

--- Array.java
+++ Array.java
@@ -9,7 +9,7 @@ public class Array {
 	 }
 	 public int occur (int key) {
 		 int count = 0;
-		 for (int i=0; i<A.length - 1; i++) 
+		 for (int i=0; i<A.length; i++) 
 			 if (A[i] == key) count++;
 		 return count;
 		 }
