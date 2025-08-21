eed3edce0a5c6383c3e0c1aadd60d2a0da1f50e2
AlRushood <AlRushood@DESKTOP-9AI2KOR>
2025-08-21 23:11:06 +0300

    change condition

--- Array.java
+++ Array.java
@@ -9,7 +9,7 @@ public class Array {
 	 }
 	 public int occur (int key) {
 		 int count = 0;
-		 for (int i=0; i<A.length + 5; i++) 
+		 for (int i=0; i<A.length ; i++) 
 			 if (A[i] == key) count++;
 		 return count;
 		 }
